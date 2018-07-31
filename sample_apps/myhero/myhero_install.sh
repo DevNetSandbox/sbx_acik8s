f575cf6ce3e56a4278025578b641c4c3af45e1c2
hpreston <hank.preston@gmail.com>
2018-07-31 13:41:18 -0400

    starting install deploy script

---
+++ sample_apps/myhero/myhero_install.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# kubectl apply -f myhero_data.yaml
+# kubectl apply -f myhero_mosca.yaml
+# kubectl apply -f myhero_ernst.yaml
+# kubectl apply -f myhero_app.yaml
+
+MYHERO_APP_EXTERNAL_IP=$(kubectl describe service myhero-app | awk '/LoadBalancer Ingress:/ {print $3}'
+172.20.4.194)
+
+echo "AP IP: ${MYHERO_APP_EXTERNAL_IP}"
