28747036e6254e4fd82a4d531b9ac7f70496c6ae
hpreston <hank.preston@gmail.com>
2018-07-31 14:08:52 -0400

    working install and uninsstall

--- sample_apps/myhero/myhero_install.sh
+++ sample_apps/myhero/myhero_install.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
-# kubectl apply -f myhero_data.yaml
-# kubectl apply -f myhero_mosca.yaml
-# kubectl apply -f myhero_ernst.yaml
-# kubectl apply -f myhero_app.yaml
+kubectl apply -f myhero_data.yaml
+kubectl apply -f myhero_mosca.yaml
+kubectl apply -f myhero_ernst.yaml
+kubectl apply -f myhero_app.yaml
 
 MYHERO_APP_EXTERNAL_IP=$(kubectl get services myhero-app -o json | python -c 'import sys, json; print json.load(sys.stdin)["status"]["loadBalancer"]["ingress"][0]["ip"]')
 
 echo "AP IP: ${MYHERO_APP_EXTERNAL_IP}"
 
-sed -i '.bak' 's/<PROVIDE-EXTERNAL-IP-FOR-myhero-app/${MYHERO_APP_EXTERNAL_IP}/' myhero_ui.yaml
+sed "s/<PROVIDE-EXTERNAL-IP-FOR-myhero-app>/${MYHERO_APP_EXTERNAL_IP}/" myhero_ui.yaml > myhero_ui_ready.yaml
+
+kubectl apply -f myhero_ui_ready.yaml
---
+++ sample_apps/myhero/myhero_uninstall.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+kubectl delete service myhero-ui
+kubectl delete service myhero-app
+kubectl delete service myhero-data
+kubectl delete service myhero-mosca
+
+kubectl delete deployment myhero-ui
+kubectl delete deployment myhero-app
+kubectl delete deployment myhero-ernst
+kubectl delete deployment myhero-mosca
+kubectl delete deployment myhero-data
