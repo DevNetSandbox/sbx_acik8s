bc6add54e395a0b9f5525b04f4ade0ce8241cad7
hpreston <hank.preston@gmail.com>
2018-07-31 13:58:51 -0400

    updating UI def

--- sample_apps/myhero/myhero_install.sh
+++ sample_apps/myhero/myhero_install.sh
@@ -5,7 +5,8 @@
 # kubectl apply -f myhero_ernst.yaml
 # kubectl apply -f myhero_app.yaml
 
-MYHERO_APP_EXTERNAL_IP=$(kubectl describe service myhero-app | awk '/LoadBalancer Ingress:/ {print $3}')
-
+MYHERO_APP_EXTERNAL_IP=$(kubectl get services myhero-app -o json | python -c 'import sys, json; print json.load(sys.stdin)["status"]["loadBalancer"]["ingress"][0]["ip"]')
 
 echo "AP IP: ${MYHERO_APP_EXTERNAL_IP}"
+
+sed -i '.bak' 's/<PROVIDE-EXTERNAL-IP-FOR-myhero-app/MYHERO_APP_EXTERNAL_IP/' myhero_ui.yaml
