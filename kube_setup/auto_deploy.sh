be53bae50d9ebed654c989700a58600b6973a4ab
hpreston <hank.preston@gmail.com>
2018-10-19 17:24:19 -0400

    Testing phased deploys

--- kube_setup/auto_deploy.sh
+++ kube_setup/auto_deploy.sh
@@ -11,18 +11,44 @@
 POD_NUM=$1
 POD_PASS=$2
 
-echo "Pod Number: ${POD_NUM}"
-echo "Pod Password: ${POD_PASS}"
+# DEPLOY_PHASE will determine which stages to deploy
+#  Stages are cumulative, running the steps before
+#    1) devbox  - Just prepare the DevBox
+#    2) network - Configure the network plumbing on Kubernetes Nodes
+#    3) prereq  - Prepare Kubernetes Nodes for cluster setup
+#    4) k8s     - Setup new Kubernetes Cluster
+#    5) cni     - Install ACI CNI plug-in to Kubernetes
+#    x) full    - Full setup (functionally same as previous stage)
+DEPLOY_PHASE=$3
 
-if [ -z ${POD_NUM} ] || [ -z ${POD_PASS} ]
+
+if [ -z ${POD_NUM} ] || [ -z ${POD_PASS} ] || [ -z ${DEPLOY_PHASE} ]
 then
   echo "You mush provide a pod number and pod password."
-  echo " ./auto_deploy.sh POD_NUM POD_PASS"
+  echo " ./auto_deploy.sh POD_NUM POD_PASS DEPLOY_PHASE"
+  echo " "
+  echo "Possible Stages are: "
+  echo "   Stages are cumulative, running the steps before "
+  echo "    1) devbox  - Just prepare the DevBox "
+  echo "    2) network - Configure the network plumbing on Kubernetes Nodes"
+  echo "    3) prereq  - Prepare Kubernetes Nodes for cluster setup "
+  echo "    4) k8s     - Setup new Kubernetes Cluster"
+  echo "    5) cni     - Install ACI CNI plug-in to Kubernetes"
+  echo "    x) full    - Full setup (functionally same as previous stage)"
   exit
 fi
 
-# 1. Basic DevBox Setup
-#
+echo "Pod Number: ${POD_NUM}"
+echo "Pod Password: ${POD_PASS}"
+
+case ${DEPLOY_PHASE} in
+  devbox|network|prereq|k8s|cni|full) echo "Deploy Stage: ${DEPLOY_PHASE}";;
+  *) echo "Requested Deploy Stage of '${DEPLOY_PHASE}' not valid"; exit;
+esac
+
+exit
+
+# Stage 1) devbox Basic DevBox Setup
 echo "Setup DevBox with Development Tools and Repos"
 sudo yum install -y wget git sshpass >> ~/auto_deploy.log 2>&1
 wget https://bootstrap.pypa.io/get-pip.py  >> ~/auto_deploy.log 2>&1
@@ -64,6 +90,7 @@ then
 fi
 echo " "
 
+# Stage 2) network - Setup Network Plumbing on Kubernetes Nodes
 echo "Run kube_network_prep.yaml"
 ansible-playbook -i inventory/sbx${POD_NUM}-hosts \
   kube_network_prep.yaml  >> ~/auto_deploy.log 2>&1
@@ -74,6 +101,7 @@ then
 fi
 echo " "
 
+# Stage 3) prereq - Install prereqs for Kubernetes and prep Linux
 echo "Run kube_prereq_install.yml"
 ansible-playbook -i inventory/sbx${POD_NUM}-hosts \
   kube_prereq_install.yml  >> ~/auto_deploy.log 2>&1
@@ -84,6 +112,7 @@ then
 fi
 echo " "
 
+# Stage 4) k8s - Setup a new Kubernetes Cluster
 echo "Run kube_install.yaml"
 ansible-playbook -i inventory/sbx${POD_NUM}-hosts \
   --extra-vars "POD_NUM=${POD_NUM}" \
@@ -95,6 +124,7 @@ then
 fi
 echo " "
 
+# Stage 5) cni - Install ACI CNI plug-in for Kubernetes
 echo "Install ACI CNI Plugin"
 cd ~/sbx_acik8s/kube_setup/aci_setup/sbx${POD_NUM}
 kubectl apply -f aci-containers.yaml  >> ~/auto_deploy.log 2>&1
